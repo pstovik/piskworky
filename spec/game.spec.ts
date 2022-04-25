e746bbc52003680313b7ffd52f38b7ad408f1867
Petr Šťovík <p.stovik@quadient.com>
2022-04-25 15:51:27 +0200

    Complex game

--- spec/game.spec.ts
+++ spec/game.spec.ts
@@ -6,6 +6,7 @@ describe("game::", () => {
         it("should be played by 'X' player", () => {
             const game = new Game();
             expect(game.currentPlayer).toBe(GameSymbol.X);
+            expect(game.gameWinner).not.toBeDefined();
         });
     });
 
@@ -16,4 +17,29 @@ describe("game::", () => {
             expect(game.currentPlayer).toBe(GameSymbol.O);
         });
     });
+
+    describe("given 6 symbols placed", () => {
+        it("next move should be played by 'X' player", () => {
+            const game = new Game();
+            playComplexGame(game);
+            expect(game.currentPlayer).toBe(GameSymbol.X);
+        });
+    });
+
+    describe("given 'X' made 3 symbols in vertical line", () => {
+        it("should mark game as won by 'X'", () => {
+            const game = new Game();
+            playComplexGame(game);
+            expect(game.gameWinner).toBe(GameSymbol.O);
+        });
+    });
 });
+
+function playComplexGame(game: Game) {
+    game.currentPlayerPutSymbol({ x: 0, y: 0 }); // X
+    game.currentPlayerPutSymbol({ x: 1, y: 1 }); // O
+    game.currentPlayerPutSymbol({ x: 0, y: 1 }); // X
+    game.currentPlayerPutSymbol({ x: 1, y: 0 }); // O
+    game.currentPlayerPutSymbol({ x: 2, y: 0 }); // X
+    game.currentPlayerPutSymbol({ x: 1, y: 2 }); // O
+}
--- src/game.ts
+++ src/game.ts
@@ -7,10 +7,21 @@ interface ICoordinates {
 
 export class Game {
     currentPlayer: GameSymbol;
+    gameWinner?: GameSymbol;
 
     constructor() {
         this.currentPlayer = GameSymbol.X;
     }
 
-    currentPlayerPutSymbol(_param: ICoordinates) {}
+    currentPlayerPutSymbol(_param: ICoordinates) {
+        // this.currentPlayer = GameSymbol.O;
+        if (this.currentPlayer === GameSymbol.X) {
+            this.currentPlayer = GameSymbol.O;
+        } else {
+            this.currentPlayer = GameSymbol.X;
+        }
+        // this.currentPlayer = this.currentPlayer === GameSymbol.X ? GameSymbol.O : GameSymbol.X;
+
+        // TODO: game logic
+    }
 }
